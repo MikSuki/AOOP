5436bb79625f7e57bbede25152ba6e0f74bd6064
MikSuki <34766025+MikSuki@users.noreply.github.com>
2021-12-06 21:57:35 +0800

    upd

---
+++ HW4/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "lexer.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    Lexer lexer;
+
+    // try
+    // {
+    //     while (true)
+    //     {
+    //         Token t = lexer.scan();
+    //         cout << t.toString();
+    //     }
+    // }
+    // catch (const char* message) {
+    //     cout << "error";
+    //     cout << message << endl;
+    // }
+    // catch (IOExeception &e)
+    // {
+    //     cout << e.what() << endl;
+    // }
+}
